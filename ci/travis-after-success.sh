61bf0c5b6b91e1e632fbea54fcf67e5ff2e06683
CurlyMoo <curlymoo1@gmail.com>
2023-02-12 08:41:19 +0100

    Exclude main from coveralls

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -5,7 +5,7 @@ if [[ "$COVERAGE" == "1" && "$TRAVIS_EVENT_TYPE" != "pull_request" ]]; then
     gem install coveralls-lcov
     lcov -c -d CMakeFiles/start.dir -o coverage.info
 
-    lcov -r coverage.info '/usr/include/*' '*log.cpp' -o coverage-filtered.info
+    lcov -r coverage.info '/usr/include/*' '*log.cpp' '*main.cpp' -o coverage-filtered.info
 
     lcov --list coverage-filtered.info
 
